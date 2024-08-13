e43150b404c72c21c3e048dc6f5493dc69a4b5c4
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-13 16:24:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_value = np.histrogram(data[DATE_COLUMN].dt.hour, bins = 24, range = (0, 24))[0]
-st.bar_chart(hist_value)
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
