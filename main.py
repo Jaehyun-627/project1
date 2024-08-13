679d1028c18b92454d8802f726616607a2087e68
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-13 16:18:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache_data
+
+
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -22,5 +25,16 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('show raw data'):
+         st.subheader('raw data)
+         st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_value = np.histrogram(data[DATE_COLUMN].dt.hour, bins = 24, range = (0, 24))[0]
+st.bar_chart(hist_value)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
