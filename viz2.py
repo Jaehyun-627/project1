dc64bfcae14db092afa745b0746154fe9f3bf119
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-19 19:38:43 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -1,3 +1,13 @@
 import streamlit as st
+
 import pandas as pd
 import numpy as np
+
+from viz2 import DATE_COLUMN'
+
+st.title('Simple Sales Data Dashboard')
+
+DATE_COLUMN = 'Product'
+
+def load_data(nrows):
+  data = pd.read_csv("sales_data.csv", nrows = nrows)
