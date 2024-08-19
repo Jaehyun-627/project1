05ae18156d93b1c9342fe49551ee3d8019274cbb
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-19 19:44:18 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -12,6 +12,6 @@ DATE_COLUMN = 'Product'
 def load_data(nrows):
   data = pd.read_csv("sales_data.csv", nrows = nrows)
   lowercase = lambda x: str(x).lower()
-  data.rename(lowercase, axis='columns', inplace=True
+  data.rename(lowercase, axis='columns', inplace=True)
   data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
 return data
