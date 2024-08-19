546f2638a74561d74d19335cf296eb4ab039c5ad
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-19 19:43:48 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from viz2 import DATE_COLUMN'
+from viz import DATE_COLUMN'
 
 st.title('Simple Sales Data Dashboard')
 
@@ -11,3 +11,7 @@ DATE_COLUMN = 'Product'
 
 def load_data(nrows):
   data = pd.read_csv("sales_data.csv", nrows = nrows)
+  lowercase = lambda x: str(x).lower()
+  data.rename(lowercase, axis='columns', inplace=True
+  data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+return data
