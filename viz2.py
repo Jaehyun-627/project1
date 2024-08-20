59943a8a4c5b01c5ffa6914a9eaa764096e8e5a7
Jaehyun-627 <jaykim2956@gmail.com>
2024-08-19 20:25:22 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -20,7 +20,7 @@ agg_data = data.groupby(['Region']).sum()
 
 # st.write(agg_data)
 
-st.bar_chart(agg_data, x = 'Region', y= 'Sales')
+st.bar_chart(agg_data, x ='Region', y='Sales')
 
 
 # df = pd.DataFrame({'Product'['Product A', 'ProductA', 'ProductA', 'ProductA', 'ProductB', 'ProductB','ProductB','ProductB'], 
