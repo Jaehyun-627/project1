752173cdb99c1d94ad66a59fbc255053a4ad3003
Jaehyun-627 <jaykim2956@gmail.com>
2024-09-07 17:24:07 -0400

    Update st01.py

--- st01.py
+++ st01.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("hello")
+st.title("Product Review Data")
+
+Data_URl = sales_data.csv
+def load_data():
+  data = pd.read_csv(Data_URL)
+  retrun data
+
+data = load_data()
+
+if st.checkbox("Show raw data"):
+  st.subheader("Raw Data")
+  st.write(data)
+
+agg_data = data.groupby(["Product"]).mean().reset_index()
+st.bar_chart(agg_data, x = "Product", y = "Review Score")
