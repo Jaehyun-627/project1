fd72c955e7c196c19482bcf2792bdabc1ce8efd5
Jaehyun-627 <jaykim2956@gmail.com>
2024-09-13 22:36:33 -0400

    Update st01.py

--- st01.py
+++ st01.py
@@ -4,14 +4,14 @@ import numpy as np
 
 st.title("Product Review Data")
 
-DATA_URL = 'Product_reviews'
+DATA_URL = 'sales_data.csv'
 def load_data():
   data = pd.read_csv(DATA_URL)
   return data
 
 data = load_data()
 if st.checkbox("Show raw data"):
-  st.subheader("Raw Data")
+  st.subheader("Raw data")
   st.write(data)
 
 agg_data = data.groupby(['Product']).sum().reset_index()
