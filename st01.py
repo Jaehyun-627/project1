a0cb0059c40d63f52f95bdbe259e446d2f7dcd9c
Jaehyun-627 <jaykim2956@gmail.com>
2024-09-13 22:31:43 -0400

    Update st01.py

--- st01.py
+++ st01.py
@@ -14,5 +14,5 @@ if st.checkbox("Show raw data"):
   st.subheader("Raw Data")
   st.write(data)
 
-agg_data = data.groupby(["Product"]).mean().reset_index()
-st.bar_chart(agg_data, x = "Product", y = "Review Score")
+agg_data = data.groupby(['Product']).sum().reset_index()
+st.bar_chart(agg_data, x='Product', y='Review Score')
